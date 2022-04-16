272c84ccd44374ba894a14893b924f508dbd5b05
ConnorTheDev <70009795+ConnorTheDev@users.noreply.github.com>
2022-04-16 22:20:57 +0100

    moved cooldown to server side

--- CTD-Cartheft/server/main.lua
+++ CTD-Cartheft/server/main.lua
@@ -1,8 +1,27 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local InCoolDown = 0
+local CoolDownTime = 1800 * 1000 --30 mins
 
 RegisterServerEvent('CTD-Cartheft:Payment')
 AddEventHandler('CTD-Cartheft:Payment', function()
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
     Player.Functions.AddMoney("cash", Config.Payment, "Payment")
+end)
+
+RegisterServerEvent('CTD-Cartheft:setcooldown')
+AddEventHandler('CTD-Cartheft:setcooldown', function()
+	InCoolDown = 1
+	Citizen.Wait(CoolDownTime)
+	InCoolDown = 0
+end)
+
+RegisterServerEvent('CTD-Cartheft:checkcooldown')
+AddEventHandler('CTD-Cartheft:checkcooldown', function()
+	local src = source
+    if InCoolDown == 0 then
+		TriggerClientEvent('CTD-Cartheft:client:SpawnCar', src)
+	else
+        TriggerClientEvent('QBCore:Notify', src, "cooldown", "error")
+    end
 end)
